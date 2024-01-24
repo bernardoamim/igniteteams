f15102c592e074363c92c3c9a32a1ca307506d2e
bernardo.amim <bernardo.amim@mercatus.com>
2024-01-24 01:26:41 -0300

    feat: adding NewGroup screen, refactoring css helper

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,11 @@
+import { TextInputProps } from 'react-native'
+import { useTheme } from 'styled-components/native'
+import { Container } from './styles'
+
+type InputProps = TextInputProps & {}
+
+export function Input({ ...rest }: InputProps) {
+  const { COLORS } = useTheme()
+
+  return <Container placeholderTextColor={COLORS.GRAY_300} {...rest} />
+}
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,17 +1,31 @@
+import { Button } from '@components/Button'
 import { GroupCard } from '@components/GroupCard'
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
+import { ListEmpty } from '@components/ListEmpty'
+import { useState } from 'react'
+import { FlatList } from 'react-native'
 import { Container } from './styles'
 
 export function Groups() {
+  const [groups, setGroups] = useState([])
+
   return (
     <Container>
       <Header />
       <Highlight title="Turmas" subtitle="Jogue com sua turma" />
 
-      <GroupCard title="Turma do Paulinho" />
-      <GroupCard title="Turma do Josi" />
-      <GroupCard title="Turma do João" />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        contentContainerStyle={!groups.length && { flex: 1 }}
+        renderItem={({ item }) => <GroupCard title={item} />}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Cadastre sua primeira turma." />
+        )}
+      />
+
+      <Button title="Criar nova turma" />
     </Container>
   )
 }
